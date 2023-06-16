57b962e41215505f86d8630b3c83c4222a41ea99
Tetiana <potapova.taty.8@gmail.com>
2023-06-16 23:48:08 +0300

    obj

--- script.js
+++ script.js
@@ -1,86 +1,78 @@
 'use strict';
 
 
-console.log( NaN && 2 && undefined );
 
 
-let numberOfFilms;
+// let numberOfFilms;
 
-function start() {
-	numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+// function start() {
+// 	numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '').trim();
 
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
-	}
-}
+// 	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+// 		numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+// 	}
+// }
 
-start();
+// start();
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-
-
-function whiteYourGenres() {
-	for (let i = 1; i <= 3; i++) {
-		personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
-	}
-
-}
-whiteYourGenres();
-
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-// 	b = prompt('На сколько оцениваете фильм?', ''),
-// 	c = prompt('Один из последних просмотренных фильмов?', ''),
-// 	d = prompt('На сколько оцениваете фильм?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
 
 
-function rememberMyFilms() {
-	for (let i = 0; i < 2; i++) {
-		const a = prompt('Один из последних просмотренных фильмов?', ''),
-			b = prompt('На сколько оцениваете фильм?', '');
 
-		if (a != null && b != null && a!= '' && b!= '' && a.length < 50) {
-			personalMovieDB.movies[a] = b;
-			console.log('done');
-		} else {
-			console.log('err');
-			i--;
-		}
-	}
-}
+// function whiteYourGenres() {
+// 	for (let i = 1; i <= 3; i++) {
+// 		personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+// 	}
+
+// }
+// whiteYourGenres();
+
+
+
+// function rememberMyFilms() {
+// 	for (let i = 0; i < 2; i++) {
+// 		const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 			b = prompt('На сколько оцениваете фильм?', '');
+
+// 		if (a != null && b != null && a!= '' && b!= '' && a.length < 50) {
+// 			personalMovieDB.movies[a] = b;
+// 			console.log('done');
+// 		} else {
+// 			console.log('err');
+// 			i--;
+// 		}
+// 	}
+// }
 // rememberMyFilms();
 
 
-function detectPersonalLevel() {
-	if (personalMovieDB.count < 10) {
-		console.log('Просмотрено довольно мало фильмов');
-	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-		console.log('Вы классический зритель');
-	} else if ( personalMovieDB.count >= 30) {
-		console.log('Вы киноман');
-	} else  {
-		console.log('Err');
-	}
-}
+// function detectPersonalLevel() {
+// 	if (personalMovieDB.count < 10) {
+// 		console.log('Просмотрено довольно мало фильмов');
+// 	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+// 		console.log('Вы классический зритель');
+// 	} else if ( personalMovieDB.count >= 30) {
+// 		console.log('Вы киноман');
+// 	} else  {
+// 		console.log('Err');
+// 	}
+// }
 
 // detectPersonalLevel();
 
-function showMyDB(hidden) {
-	if (!hidden) {
-		console.log(personalMovieDB);
-	}
-}
+// function showMyDB(hidden) {
+// 	if (!hidden) {
+// 		console.log(personalMovieDB);
+// 	}
+// }
 
-showMyDB(personalMovieDB.privat);
+// showMyDB(personalMovieDB.privat);
 
 
 
@@ -110,32 +102,32 @@ showMyDB(personalMovieDB.privat);
 
 // }
 
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
 
 
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-	console.log('Все сыты!');
-} else {
-	console.log('Мы уходим!');
-}
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+// 	console.log('Все сыты!');
+// } else {
+// 	console.log('Мы уходим!');
+// }
 
-let johnReport, alexReport, samReport, mariaReport = 'done';
+// let johnReport, alexReport, samReport, mariaReport = 'done';
 
-console.log(johnReport || alexReport || samReport || mariaReport);
-
-
-console.log( 5 === 5 && 3 > 1 );
+// console.log(johnReport || alexReport || samReport || mariaReport);
 
 
-if (hamburger && cola || fries === 3 && nuggets) {
-	console.log('Done!');
-} else {
-	console.log('NO');
-}
-console.log(3 && 2);
+// console.log( 5 === 5 && 3 > 1 );
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+// 	console.log('Done!');
+// } else {
+// 	console.log('NO');
+// }
+// console.log(3 && 2);
 
 
 
@@ -165,78 +157,78 @@ console.log(3 && 2);
 // *****
 // ******
 
-let resultStar = '';
-const lengthStar = 7;
+// let resultStar = '';
+// const lengthStar = 7;
 
-for (let i = 1; i < lengthStar; i++) {
+// for (let i = 1; i < lengthStar; i++) {
 
-	for (let j = 0; j < i; j++) {
-		resultStar += '*';
-	}
-	resultStar += '\n';
+// 	for (let j = 0; j < i; j++) {
+// 		resultStar += '*';
+// 	}
+// 	resultStar += '\n';
 
-}
-console.log(resultStar);
-
-
-first: for (let i = 0; i < 3; i++) {
-	console.log(`First level: ${i}`);
-	for (let j = 0; j < 3; j++) {
-		console.log(`Second level: ${j}`);
-		for (let k = 0; k < 3; k++) {
-			if (k === 2) break first;
-			console.log(`Third level: ${k}`);
-		}
-	}
-}
+// }
+// console.log(resultStar);
 
 
-const arrayOfNumbers = [];
+// first: for (let i = 0; i < 3; i++) {
+// 	console.log(`First level: ${i}`);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(`Second level: ${j}`);
+// 		for (let k = 0; k < 3; k++) {
+// 			if (k === 2) break first;
+// 			console.log(`Third level: ${k}`);
+// 		}
+// 	}
+// }
+
+
+// const arrayOfNumbers = [];
 
 // Пишем решение вот тут
     
-for (let i = 5; i < 11; i++) {
-	arrayOfNumbers[i - 5] = i;
-}  
+// for (let i = 5; i < 11; i++) {
+// 	arrayOfNumbers[i - 5] = i;
+ 
     
 // Не трогаем
-console.log(arrayOfNumbers[0]); 
+// console.log(arrayOfNumbers[0]); 
 
 
-var i = 1;
+// var i = 1;
 
-while (i < 6) {
-	if (i == 3) {
-		break;
-	}
-	i++;
-}
+// while (i < 6) {
+// 	if (i == 3) {
+// 		break;
+// 	}
+// 	i++;
+// }
 
-console.log(i + 2);
+// console.log(i + 2);
 
-i = 0;
-while (i < 5) {
-	i++;
-	if (i === 3) {
-		continue;
-	}
-	console.log(i);
-}
+// i = 0;
+// while (i < 5) {
+// 	i++;
+// 	if (i === 3) {
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
 
-const lines = 10;
-let results = '';
+// const lines = 10;
+// let results = '';
 
-for (let i = 0; i <= lines; i++) {
-	for (let j = 0; j < lines - i; j++) {
-		results += ' ';
-	}
-	for (let j = 0; j < 2 * i + 1; j++) {
-		results += '*';
-	}
-	results += '\n';
-}
+// for (let i = 0; i <= lines; i++) {
+// 	for (let j = 0; j < lines - i; j++) {
+// 		results += ' ';
+// 	}
+// 	for (let j = 0; j < 2 * i + 1; j++) {
+// 		results += '*';
+// 	}
+// 	results += '\n';
+// }
 
-console.log(results);
+// console.log(results);
 
 
 
@@ -251,81 +243,283 @@ console.log(results);
 // console.log(calc(5, 7));
 
 
-function ret() {
-	let num = 50;
-	return num;
-}
-const anotherNum = ret();
-console.log(anotherNum);
+// function ret() {
+// 	let num = 50;
+// 	return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
 
 
-const logger = function() {
-	console.log('Hello');
+// const logger = function() {
+// 	console.log('Hello');
+// };
+
+// logger();
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+
+// function convert(amount, curr) {
+// 	return curr * amount;
+// }
+
+// function promotion(result) {
+// 	console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+// const strng = 'test';
+// const arr = [1, 2, 4];
+
+// console.log(arr.length);
+// console.log(strng.toLocaleUpperCase());
+
+// const fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('q'));
+
+// const logg = 'Hello world';
+
+// console.log(logg.slice(0, -5));
+// console.log(logg.substring(0, 6));
+
+// const num = 12.5;
+// console.log(Math.round(num));
+
+// const test = '12.6px';
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+// function fib(num) {
+    
+// 	if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+// 		return '';
+// 	}
+
+// 	let result = '';
+// 	let first = 0;
+// 	let second = 1;
+
+// 	for (let i = 0; i < num; i++) {
+// 		if (i + 1 === num) {
+// 			result += `${first}`;
+        
+// 		} else {
+// 			result += `${first} `;
+// 		}
+
+// 		let third = first + second;
+// 		first = second;
+// 		second = third;
+// 	}
+
+// 	return console.log(result);
+// }
+
+// fib(9);
+
+// function first() {
+// 	setTimeout(function () {
+// 		console.log(1);
+// 	}, 500);
+// }
+
+// first();
+
+// function learnJS (lang, callback) {
+// 	console.log(`Я учу: ${lang}`);
+// 	callback();
+// }
+
+// function done () {
+// 	console.log('Я прошел этот урок');
+// }
+
+// learnJS('Java Script', done);
+
+// Обьекты
+
+// const options = {
+// 	name: 'test',
+// 	width: 1024,
+// 	height: 1024,
+// 	colors: {
+// 		border: 'black',
+// 		bg: 'red'
+// 	}
+// };
+
+// console.log(Object.keys(options).length);
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+
+
+// let counter = 0;
+// for (let key in options) {
+// 	if (typeof(options[key]) === 'object') {
+// 		for (let i in options[key]) {
+// 			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+// 			counter++;
+// 		}
+// 	} else {
+// 		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// 		counter++;
+// 	}
+	
+// }
+// console.log(counter);
+
+
+const arriv = [1, 2, 3, 4, 8];
+
+arriv.forEach(function(item, i, arriv) {
+	console.log(`${i}: ${item} внутри массива ${arriv}`);
+});
+
+// arriv.pop();
+// arriv.push(10);
+// console.log(arriv);
+
+// for (let i = 0; i < arriv.length; i++) {
+// 	console.log(arriv[i]);
+// }
+// for (let value of arriv) {
+// 	console.log(value);
+// }
+
+// const str = prompt('', '');
+// const products = str.split(', ');
+// products.sort();
+// console.log(products.join(';'));
+
+// const arr = [2, 444, 15, 35, 88];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+// 	return a - b;
+// }
+
+
+// const obj = {
+// 	a: 5,
+// 	b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+// 	let objCopy = {};
+
+// 	let key;
+// 	for (key in mainObj) {
+// 		objCopy[key] = mainObj[key];
+// 	}
+// 	return objCopy;
+// }
+
+// const numbers = {
+// 	a: 2,
+// 	b: 4,
+// 	c: {
+// 		x: 6,
+// 		y: 3
+// 	}
+// };
+
+// const newNumbers = copy(numbers);
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// // console.log(newNumbers);
+// // console.log(numbers);
+
+// const add = {
+// 	d: 17,
+// 	e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[2] = 'kslkdngsldkgsl';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+// blogs = ['wordpress', 'livejournal', 'blogger'],
+// internet = [...video, ...blogs, 'vk', 'facebook'];
+// console.log(internet);
+
+// function log (a, b, c) {
+// 	console.log(a);
+// 	console.log(b);
+// 	console.log(c);
+// }
+
+// const num = [2, 5, 7];
+// log (...num);
+
+const array = ['a', 'b'];
+
+const newArray = [...array];
+
+console.log(newArray);
+
+const q = {
+	one: 1,
+	two: 2,
+	three: {
+		four: 4
+	}
 };
 
-logger();
-
-const usdCurr = 28;
-const discount = 0.9;
+const newObj = {...q};
+console.log(newObj);
 
 
-function convert(amount, curr) {
-	return curr * amount;
-}
 
-function promotion(result) {
-	console.log(result * discount);
-}
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
-const res = convert(500, usdCurr);
-promotion(res);
+function availableCurr(arr, missingCurr) {
+	let str = '';
+	arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
 
-const strng = 'test';
-const arr = [1, 2, 4];
-
-console.log(arr.length);
-console.log(strng.toLocaleUpperCase());
-
-const fruit = 'Some fruit';
-
-console.log(fruit.indexOf('q'));
-
-const logg = 'Hello world';
-
-console.log(logg.slice(0, -5));
-console.log(logg.substring(0, 6));
-
-const num = 12.5;
-console.log(Math.round(num));
-
-const test = '12.6px';
-console.log(parseInt(test));
-console.log(parseFloat(test));
-
-function fib(num) {
-    
-	if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-		return '';
-	}
-
-	let result = '';
-	let first = 0;
-	let second = 1;
-
-	for (let i = 0; i < num; i++) {
-		if (i + 1 === num) {
-			result += `${first}`;
-        
-		} else {
-			result += `${first} `;
+	arr.forEach(function(curr) {
+		if (curr !== missingCurr) {
+			str += `${curr}\n`;
 		}
+	});
 
-		let third = first + second;
-		first = second;
-		second = third;
-	}
+	// Или
+	// for (let i = 0; i < arr.length; i++) {
+	//     if (arr[i] === missingCurr) {
+	//         continue;
+	//     }
+	//     str += `${arr[i]}\n`;
+	// }
 
-	return console.log(result);
+	return str;
 }
 
-fib(9);
+
+
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
