b846519dbfe85d583d91b4d413747e8b507977e3
Tetiana <potapova.taty.8@gmail.com>
2024-01-09 06:54:56 +0200

    add text in html

--- script.js
+++ script.js
@@ -604,23 +604,101 @@
 // isBudgetEnough(shoppingMallData);
 
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard',  'Sam'];
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard',  'Sam'];
 
-function sortStudentsByGroups(arr) {
-	arr.sort();
-	const a = [], b = [], c = [], rest = [];
+// function sortStudentsByGroups(arr) {
+// 	arr.sort();
+// 	const a = [], b = [], c = [], rest = [];
 
-	for (let i = 0; i < arr.length; i++) {
-		if (i < 3) {
-			a.push(arr[i]);
-		} else if (i < 6) {
-			b.push(arr[i]);
-		} else if (i < 9) {
-			c.push(arr[i]);
-		} else {
-			rest.push(arr[i]);
-		}
-	}
-	return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
-}
-console.log(sortStudentsByGroups(students));
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (i < 3) {
+// 			a.push(arr[i]);
+// 		} else if (i < 6) {
+// 			b.push(arr[i]);
+// 		} else if (i < 9) {
+// 			c.push(arr[i]);
+// 		} else {
+// 			rest.push(arr[i]);
+// 		}
+// 	}
+// 	return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
+// }
+// console.log(sortStudentsByGroups(students));
+
+// const restorantData = {
+// 	menu: [
+// 		{
+// 			name: 'Salad Caesar',
+// 			price: '14$'
+// 		},
+// 		{
+// 			name: 'Pizza Diavola',
+// 			price: '9$'
+// 		},
+// 		{
+// 			name: 'Beefsteak',
+// 			price: '17$'
+// 		},
+// 		{
+// 			name: 'Napoleon',
+// 			price: '7$'
+// 		}
+// 	],
+// 	waitors: [
+// 		{name: 'Alice', age: 22}, {name: 'John', age: 24}
+// 	],
+// 	averageLunchPrice: '20$',
+// 	openNow: true
+// };
+
+// const {openNow} = restorantData;
+
+// function isOpen(prop) {
+// 	let answer = '';
+// 	prop ? answer = 'открыто' : answer = 'закрыто';
+
+// 	return answer;
+// }
+
+// console.log(isOpen(openNow));
+
+// function isAverageLunchPriceTrue(fDish, sDish, average) {
+// 	if (+fDish.price.slice(0, -1) + (sDish.price) < average) {
+// 		return 'Цена ниже средней';
+// 	} else {
+// 		return 'Цена выше средней';
+// 	}
+// }
+
+// console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+// function transferWaitors(data) {
+// 	const copy = Object.assign({}, data);
+
+// 	copy.waitors[0] = {name: 'Mike', age: 32};
+// 	return copy;
+// }
+
+// transferWaitors(restorantData);
+
+// var str = 'невероятно';
+// var str5 = str.slice(0, -1);
+// console.log(str5);
+
+
+// function pow(x, n) {
+// 	let result = 1;
+
+// 	for (let i = 0; i < n, i++) {
+// 		result *= x
+// 	}
+// 	return result;
+// }
+  
+// const k = Math.pow(2, 2);
+
+// console.log(k);
+
+var str = "фывапролд";
+var strReverse = str.split('');
+console.log(strReverse);
